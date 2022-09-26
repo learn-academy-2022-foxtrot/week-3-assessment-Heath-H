5384a0bb0a3e7c00d4f3cb64c01a6ba4eb46605a
Heath-H <heathhaupt92@gmail.com>
2022-09-26 08:14:08 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,54 +14,181 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// const fibLength1 = 6
+// // Expected output: [1, 1, 2, 3, 5, 8]
+
+// const fibLength2 = 10
+// // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+
+// describe ("fibSequence"), () => {
+//       //it method describes what function does
+//       it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+//           const fibLength1 = 6
+//           //Expected output: [1, 1, 2, 3, 5, 8]
+//           const fibLength2 = 10
+//           //Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+          
+          
+//       //expect calls on function and compares result to determined expected output using .toEqual
+//       expect(fibSequence(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//       expect(fibSequence(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+//       }
+//       )}
+      
+     
+//       console.log(fibSequence(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+
+     // ReferenceError: describe is not defined
+
+
+
 
 // b) Create the function that makes the test pass.
+
+//PsudoCode:
+// create a function (fibSequence)
+// that takes in a input ()
+// if input is greater than 2 (if i >2)
+// use a for loop?
+
+// add numbers together and push to new 
+// repeat (index) times
+//number must be greater than 2 though
+
+// const fibSequence = () => {
+//   const seqArray = [];
+
+//   let num1 = 1;
+//   let num2 = 1;
+
+//   for (let i = 0; i < num; i++) {
+
+//     let numAdd = num1 + num2;
+
+
+//     seqArray.push(num1);
+
+
+//     num1 = num2;
+//     num2 = numAdd;
+//   }
+
+//   return seqArray;
+
+// };
+
+
+
+
+
+
+
+
 
 // --------------------2) Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+describe("studyTime", () => {
+  it("Create a function that takes in an object and returns an array of the numbers sorted from least to greatest.", () => {
+    const studyMinutesWeek1 = {
+      sunday: 90,
+      monday: 30,
+      tuesday: 20,
+      wednesday: 15,
+      thursday: 30,
+      friday: 15,
+      saturday: 60,
+    };
+    expect(studyTime(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90]);
+    const studyMinutesWeek2 = {
+      sunday: 100,
+      monday: 10,
+      tuesday: 45,
+      wednesday: 60,
+      thursday: 20,
+      friday: 15,
+      saturday: 65,
+    };
+    expect(studyTime(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100]);
+  });
+});
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
 
-// b) Create the function that makes the test pass.
 
-// --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
-// a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+// // b) Create the function that makes the test pass.
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
+// Psuedocode:
+// need to declare the function (studyTime)
+// iterate over the funtion to get the values: objects.values.
+// .sort() method to sort through numbers least to greatest, 
 
-const accountTransactions3 = []
-// Expected output: []
 
-// b) Create the function that makes the test pass.
+const studyTime = (obj) => {
+  let newArray = [];
+
+  for (let val in obj) {
+    newArray.push(obj[val]);
+    newArray = sortedArr.sort((a, b) => a - b);
+  }
+  return newArray;
+
+};
+
+
+
+
+
+
+// // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+// // a) Create a test with expect statements for each of the variables provided.
+
+describe("accuArr", () => {
+  it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+    const accountTransactions1 = [100, -17, -23, -9];
+    expect(accuSum(accountTransactions1)).toEqual([100, 83, 60, 51]);
+    const accountTransactions2 = [250, -89, 100, -96];
+    expect(accuSum(accountTransactions2)).toEqual([250, 161, 261, 165]);
+    const accountTransactions3 = [];
+    expect(accuSum(accountTransactions3)).toEqual([]);
+  });
+});
+
+
+
+
+// const accountTransactions1 = [100, -17, -23, -9]
+// // Expected output: [100, 83, 60, 51]
+
+// const accountTransactions2 = [250, -89, 100, -96]
+// // Expected output: [250, 161, 261, 165]
+
+// const accountTransactions3 = []
+// // Expected output: []
+
+// // b) Create the function that makes the test pass.
+
+// create function accuArr
+// create if statement if array is empty to return empty array using arr.length equal to 0 ( === 0) to return [], and an else for the rest of 
+// then create new let condition with an array holding first value at[0]
+// create for loop to iterate after the first value 
+// sum the value of newArray
+// return the newArray
+
+const accuArr= (arr) => {
+  if (arr.length === 0) {
+    return []
+  } else {
+  let newArray = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    newArray[i] = newArray[i - 1] + arr[i];
+  }
+  return newArray;
+}}
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,33 +8,33 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX connects JS to HTML
 
-Researched answer:
+Researched answer: JSX stands for JavaScript Syntax Extension, which is an extension of JavaScript used in React, that will allow you to use HTML in JavaScript. you use className in JSX vs using class in HTML
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-Your answer:
+Your answer: Yarn is the command used to create a React app, th  json file is modified
 
-Researched answer:
+Researched answer: Yarn stands for Yet Another Resource Negotiator, and it is a package manager that was created by Facebook. Yarn will create a json paackage and a yarn.lock package is created as well when yarn is being ran
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: Anonymous functions do not have a name yet and are contained in ()
 
-Researched answer:
+Researched answer: These are functions that have not been declared yet, and can be used as an arguement for another function
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: state values and props in React are similar, they are both js objects. Props can get passed into a a component, and a state is inside of a component.
 
-Researched answer:
+Researched answer: Both props and state values are objects in JavaScript. Props are used to pass data from components and state values are controlled by the component itself and states are used to access props.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM is what the user sees, for example, boxes that you hover over that tell you what to type in a box, prompts, etc
 
-Researched answer:
+Researched answer: Document object model, a visual representation of your code
 
 6. STRETCH: Which is the difference between a div and a span?
 
@@ -44,12 +44,12 @@ Researched answer:
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: A programming method designed around objects and classes
 
-2. Ruby:
+2. Ruby: Ruby is a general purpose programming language used for back-end development. It is object-oriented
 
-3. Implicit return:
+3. Implicit return: This returns a method without being called
 
-4. Ruby blocks:
+4. Ruby blocks: they are anonymous functions that can be passed into methods
 
-5. Ruby hashes:
+5. Ruby hashes: a dictionary-like container for unique keys and their corresponding values.
